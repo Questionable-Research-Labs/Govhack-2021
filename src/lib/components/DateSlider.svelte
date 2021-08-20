d6f21f0fcfb8e6b7b46df00831f48657769e2c59
Jasper M-W <jasper@qrl.nz>
2021-08-21 02:35:19 +1200

    Slider now actually works as a slider

--- src/lib/components/DateSlider.svelte
+++ src/lib/components/DateSlider.svelte
@@ -1,14 +1,14 @@
 <script>
 	import RangeSlider from "svelte-range-slider-pips";
-	export let values = [ new Date("2021-07-10").getTime(), new Date().getTime()];
+	export let values = [ new Date("2021-07-10").getTime()/86400000, new Date().getTime()/86400000];
 	
 
 	function displayDates(value,pip_index) {
-		return new Date(value).toISOString().split("T")[0]
+		return new Date(value*86400000).toISOString().split("T")[0]
 	}
   </script>
 
-  <RangeSlider   bind:values min={new Date("2021-08-10").getTime()} max={new Date().getTime()} pushy range float pips step=86400000  all="label" formatter={displayDates} handleFormatter={displayDates}/>
+  <RangeSlider   bind:values min={new Date("2021-08-10").getTime()/86400000} max={new Date().getTime()/86400000} pushy range float pips  all="label" formatter={displayDates} handleFormatter={displayDates} springValues={{ stiffness: 0.5, damping: 0.5 }}/>
   <style lang="scss">
 
   </style>
