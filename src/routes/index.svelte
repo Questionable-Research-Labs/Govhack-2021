3d395f7b15364490a9fa0d370d00ef87574f6c3e
Jasper M-W <jasper@qrl.nz>
2021-08-21 00:41:13 +1200

    Copypasta map

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-    // import Map from "../lib/components/leaflet/Map.svelte";
+    import LeafletMap from "../lib/components/leaflet/LeafletMap.svelte";
+
 </script>
 
 
 <main>
     <h1>Would ya look at that</h1>
     <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-    <!-- <Map/> -->
+    <LeafletMap/>
 </main>
 
 
