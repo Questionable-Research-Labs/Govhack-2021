45147b6c8ecb35eb34912c81d3cb910f0493fac7
Jasper M-W <jasper@qrl.nz>
2021-08-21 02:29:47 +1200

    Export values

--- src/lib/components/DateSlider.svelte
+++ src/lib/components/DateSlider.svelte
@@ -1,11 +1,11 @@
 <script>
 	import RangeSlider from "svelte-range-slider-pips";
-	let values = [ new Date("2021-07-10").getTime(), new Date().getTime()];
+	export let values = [ new Date("2021-07-10").getTime(), new Date().getTime()];
+	
 
 	function displayDates(value,pip_index) {
 		return new Date(value).toISOString().split("T")[0]
 	}
-
   </script>
 
   <RangeSlider   bind:values min={new Date("2021-08-10").getTime()} max={new Date().getTime()} pushy range float pips step=86400000  all="label" formatter={displayDates} handleFormatter={displayDates}/>
