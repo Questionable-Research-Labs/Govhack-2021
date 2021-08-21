3eb716bd90ebf7b815748c6d3393abd63bfd6949
Nathan Hare <me@laspruca.nz>
2021-08-21 20:52:26 +1200

    Make the filter results bar more responsive

--- src/lib/components/SearchBox.svelte
+++ src/lib/components/SearchBox.svelte
@@ -1,39 +1,37 @@
-<script lang="ts">
-    import {GeoData} from "$lib/geoJsonResponse";
-    import {afterUpdate} from "svelte";
-    import {Place} from "$lib/place";
-    import FuzzySearch from "fuzzy-search";
+<script lang='ts'>
+	import { GeoData } from '$lib/geoJsonResponse';
+	import { afterUpdate } from 'svelte';
+	import { Place } from '$lib/place';
+	import FuzzySearch from 'fuzzy-search';
 
-    export let geoData: GeoData | null;
-    export let probablePlaces: ((places: Place[]) => void);
-    let places: Place[] = [];
+	export let geoData: GeoData | null;
+	export let probablePlaces: ((places: Place[]) => void);
+	let places: Place[] = [];
 
-    $: places = geoData?.features.map((feature, index) => new Place(index, feature.properties.event + ", " + feature.properties.location));
+	$: places = geoData?.features.map((feature, index) => new Place(index, feature.properties.event + ', ' + feature.properties.location));
 
-    let searchTerm = "";
+	let searchTerm = '';
 
-    let fuse: FuzzySearch<Place>;
-    $: fuse = new FuzzySearch(places, ['location'], {
-        caseSensitive: false
-    });
+	let fuse: FuzzySearch<Place>;
+	$: fuse = new FuzzySearch(places, ['location'], {
+		caseSensitive: false
+	});
 
-    afterUpdate(() => {
-        if (searchTerm === "") {
-            probablePlaces(places);
-        } else {
-            probablePlaces(fuse.search(searchTerm));
-        }
-    });
+	afterUpdate(() => {
+		if (searchTerm === '') {
+			probablePlaces(places);
+		} else {
+			probablePlaces(fuse.search(searchTerm));
+		}
+	});
 </script>
 
-<div class="wrapper">
-    <label>Address/Location
-        <input type="text" bind:value={searchTerm}>
-        <span></span>
-    </label>
+<div class='wrapper'>
+	<label>Address/Location</label>
+	<input type='text' bind:value={searchTerm}>
 </div>
 
-<style lang="scss">
+<style lang='scss'>
   .wrapper {
     display: flex;
     align-items: center;
@@ -42,11 +40,26 @@
   }
 
   input {
-      padding: 1em;
-      margin-left: 1em;
+    padding: 1em;
+    margin-left: 1em;
+  }
+  @media all and (max-width: 770px) {
+		.wrapper {
+      padding: 1em 0;
+		}
   }
 
-  input:required + span:after {
-    content: "Required";
+  @media only screen and (max-width: 380px) {
+    .wrapper {
+      flex-flow: column;
+    }
+
+    label {
+      padding-bottom: 0.5em;
+    }
+
+    input {
+      padding: 0.5em;
+    }
   }
 </style>
