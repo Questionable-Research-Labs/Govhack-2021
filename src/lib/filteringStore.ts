66fb05964e258db49cb9719ef98be94d2919e3c8
Nathan Hare <me@laspruca.nz>
2021-08-21 15:30:48 +1200

    Fix issue with Moment.JS

--- src/lib/filteringStore.ts
+++ src/lib/filteringStore.ts
@@ -1,5 +1,5 @@
-import { MS_IN_DAY } from "$lib/consts";
-import moment, {isMoment, Moment} from 'moment'
+import { MS_IN_DAY } from '$lib/consts';
+import moment, { Moment } from 'moment';
 
 type RelevantTimes = [number, number];
 
@@ -11,20 +11,18 @@ export enum dateRangeTimings {
 
 let markerStore: Map<number, RelevantTimes> = new Map();
 
-export const timeFromMoment = (date: Date|Moment) => {
-    let utcTime;
-    if (isMoment(date)) {
-        utcTime = date.valueOf();
-    } else {
-        utcTime = date.getTime();
-    }
-    return Math.round( utcTime / MS_IN_DAY)
+export const timeFromMoment = (date: Date | Moment) => {
+	let utcTime;
+	if (moment.isMoment(date)) {
+		utcTime = date.valueOf();
+	} else {
+		utcTime = date.getTime();
+	}
+	return Math.round(utcTime / MS_IN_DAY);
 };
 
-
-
-export function StoreMarker(times: RelevantTimes,leafletID: number) {
-    markerStore[leafletID] = times
+export function StoreMarker(times: RelevantTimes, leafletID: number) {
+	markerStore[leafletID] = times;
 }
 
 export function TestRange(dateRange: RelevantTimes, leafletID: number): dateRangeTimings {
