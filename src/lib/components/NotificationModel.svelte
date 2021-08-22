8718b557873826d800d32921af27fb5dc9016039
Jacob <jacobtread@gmail.com>
2021-08-22 13:44:40 +1200

    Started notification model stuff and fix bell not go brr

--- src/lib/components/NotificationModel.svelte
+++ src/lib/components/NotificationModel.svelte
@@ -1,103 +1,131 @@
-<script lang="ts">
-	import {getMessagingToken,deregisterForPushNotifications  } from "$lib/firebase/initFirebase"
-	import {notificationSettings} from "$lib/store";
+<script lang='ts'>
+	import { deregisterForPushNotifications, getMessagingToken } from '$lib/firebase/initFirebase';
+	import { notificationSettings } from '$lib/store';
 
-	setInterval(updatePermissionStatus,1000)
+	setInterval(updatePermissionStatus, 1000);
 	let notificationsEnabled: boolean = false;
 	let notificationDenied = false;
 
-	notificationSettings.subscribe((value)=>{
-		notificationsEnabled = value&&(Notification.permission ==="granted")
-	})
+	notificationSettings.subscribe((value) => {
+		notificationsEnabled = value && (Notification.permission === 'granted');
+	});
 
-	let errorText = "";
+	let errorText = '';
 
-	
-	
+
 	function updatePermissionStatus() {
-		if (notificationSettings && Notification.permission !=="granted") {
+		if (notificationSettings && Notification.permission !== 'granted') {
 			// User has disabled notifications
-			notificationSettings.set(false)
+			notificationSettings.set(false);
 		}
-		if (Notification.permission ==="denied") {
-			errorText = "Notifications have been blocked, check site permissions"
+		if (Notification.permission === 'denied') {
+			errorText = 'Notifications have been blocked, check site permissions';
 		}
-		notificationDenied = Notification.permission ==="denied"
+		notificationDenied = Notification.permission === 'denied';
 	}
 
 	function enableNotifications() {
-		Notification.requestPermission(function (status) {
-		    if (Notification.permission ==="granted") {
-				getMessagingToken()
+		Notification.requestPermission(function(status) {
+			if (Notification.permission === 'granted') {
+				getMessagingToken();
 				notificationSettings.set(true);
 			} else {
-				errorText = "Permission Denied"
+				errorText = 'Permission Denied';
 			}
 		});
 	}
 
 	function disableNotifications() {
-		console.log("Disabling Notifications")
+		console.log('Disabling Notifications');
 		deregisterForPushNotifications();
 		notificationSettings.set(false);
-		console.log("Disabled Notifications")
+		console.log('Disabled Notifications');
 	}
 </script>
 
 <div>
 	<h3>Notifications</h3>
 	<p>
-		Enable notifiactions to do things wow text here lol I can't english Lorem ipsum dolor sit amet
-		consectetur, adipisicing elit. Voluptatibus esse obcaecati enim, expedita inventore facere unde
-		adipisci, dolore, quaerat reiciendis culpa distinctio debitis laborum explicabo! Aperiam aliquid
-		rem soluta unde.
-
-		<br />
-		<br />
-
-		Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus esse obcaecati enim,
-		expedita inventore facere unde adipisci, dolore, quaerat reiciendis culpa distinctio debitis
-		laborum explicabo! Aperiam aliquid rem soluta unde. Lorem ipsum dolor sit amet consectetur,
-		adipisicing elit. Voluptatibus esse obcaecati enim, expedita inventore facere unde adipisci,
-		dolore, quaerat reiciendis culpa distinctio debitis laborum explicabo! Aperiam aliquid rem
-		soluta unde.
-		<br />
-		<br />
-
-		Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus esse obcaecati enim,
-		expedita inventore facere unde adipisci, dolore, quaerat reiciendis culpa distinctio debitis
-		laborum explicabo! Aperiam aliquid rem soluta unde.
+		Enable notifications within out app to be notified whenever new locations of interest
+		appear. We will send a notification straight to your device alerting you to how many
+		new locations have appeared.
 	</p>
-	<div class="buttonContainer">
-		{#if notificationsEnabled} 
-			<button class="actionButton" on:click={disableNotifications}>Disable notifications</button>
+	<div class='fake-notif'>
+		<h3 class='fake-notif__title'>New COVID-19 Locations of interest</h3>
+		<p class='fake-notif__body'>There are 11 new COVID-19 locations of interest, check the app to see if there are any
+			around you.</p>
+		<span class='fake-notif__time'>1:38 pm <span class='dot'></span> toi.qrl.nz</span>
+	</div>
+	<div class='buttonContainer'>
+		{#if notificationsEnabled}
+			<button class='actionButton' on:click={disableNotifications}>Disable notifications</button>
 		{:else}
-			<button class="actionButton" on:click={enableNotifications} disabled={notificationDenied}>Enable notifications</button>
+			<button class='actionButton' on:click={enableNotifications} disabled={notificationDenied}>Enable notifications
+			</button>
 		{/if}
 	</div>
-	<div class="error">{errorText}</div>
+	<div class='error'>{errorText}</div>
 </div>
 
-<style lang="scss">
-	.actionButton {
-		background-color: #333;
-		color: white;
-		border-radius: 5px;
-		padding: 0.5rem;
-		border: none;
-		font-size: 1.5rem;
-		&:disabled {
-			background-color: rgb(112, 112, 112);
-		}
-	}
-	.buttonContainer {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		width: 100%;
-	}
-	.error {
-		color: rgb(189, 5, 5);
-		text-align: center;
-	}
+<style lang='scss'>
+
+  .fake-notif {
+    padding: 1em;
+    background: #333333;
+
+    &__title {
+      font-size: 1em;
+      margin: 0;
+      margin-bottom: 0.5em;
+      color: white;
+    }
+
+    &__body {
+      margin: 0;
+      color: gray;
+			line-height: 1.5;
+    }
+
+    &__time {
+      display: block;
+      color: gray;
+      margin-top: 0.7em;
+      font-size: 0.8em;
+
+      .dot {
+        display: inline-block;
+        width: 4px;
+        height: 4px;
+        background: gray;
+        border-radius: 16px;
+        margin-bottom: 2px;
+      }
+
+    }
+  }
+
+  .actionButton {
+    background-color: #333;
+    color: white;
+    border-radius: 5px;
+    padding: 0.5rem;
+    border: none;
+    font-size: 1.5rem;
+
+    &:disabled {
+      background-color: rgb(112, 112, 112);
+    }
+  }
+
+  .buttonContainer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+  }
+
+  .error {
+    color: rgb(189, 5, 5);
+    text-align: center;
+  }
 </style>
