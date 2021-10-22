20c10e47e205bea78b19a5b3fc460da3e7e1fa6c
Jasper M-W <jasper@ngita.co.nz>
2021-10-23 00:07:57 +1300

    Added fancy tooltip to share button

--- src/lib/components/LeafletMap.svelte
+++ src/lib/components/LeafletMap.svelte
@@ -64,12 +64,23 @@
 			</style>	
 			`
 		}
-		// Share icon
-		output += `<button onclick="navigator.clipboard.writeText(window.location.href+ '?marker=${dataTable.id}');" class="leaflet-container leaflet-popup-close-button leaflet-popup-share-button">
+		// Share Button
+		let copyPrompt = "Copy URL";
+		let copiedPrompt = "Copied!";
+		let buttonJS = `
+			navigator.clipboard.writeText(window.location.href+ '?marker=${dataTable.id}');
+			var shareButton = document.getElementById('leaflet-popup-share-button');
+			shareButton.ariaLabel='${copiedPrompt}';
+			shareButton.addEventListener('mouseout', function( e ) {setTimeout(function() {e.target.ariaLabel = '${copyPrompt}'}, 300);});
+
+		`;
+		output += `<span class="leaflet-popup-share-button-wrapper">
+		<button id="leaflet-popup-share-button" onclick="${buttonJS}" class="leaflet-container leaflet-popup-close-button leaflet-popup-share-button" aria-label="${copyPrompt}">
 						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share-fill" viewBox="0 0 16 16">
 							<path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z"/>
 						</svg>
-					</button>`
+					</button>
+					</span>`
 		// Title
 		output += `<p>${dataTable.event}</p>`;
 
