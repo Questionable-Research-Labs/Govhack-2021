15ae7caee133d6891d172e11f061a846c2b0ec3e
Jasper Miller-Waugh <contact@jmw.nz>
2021-10-23 16:29:38 +1300

    Mobile sharing implemented

--- src/lib/components/MapHeader.svelte
+++ src/lib/components/MapHeader.svelte
@@ -1,5 +1,4 @@
-<script lang='ts'>
-
+<script lang="ts">
 	import type { Writable } from 'svelte/store';
 	import type { Tweened } from 'svelte/motion';
 
@@ -41,50 +40,42 @@
 	}
 
 	const dateToTime = (date: Date): number => Math.round(date.getTime() / MS_IN_DAY);
-	const pickStartCallback = () => dates[0] = dateToTime(startDate);
-	const pickEndCallback = () => dates[1] = dateToTime(endDate);
-
+	const pickStartCallback = () => (dates[0] = dateToTime(startDate));
+	const pickEndCallback = () => (dates[1] = dateToTime(endDate));
 </script>
 
 <header>
-	<div class='block--first'>
-		<a class='logo' href='/' title='Times of Interest'>
-			<img class='logo__image' src='/icons/icon.svg' alt='Toi' width='128' height='128' />
+	<div class="block--first">
+		<a class="logo" href="/" title="Times of Interest">
+			<img class="logo__image" src="/icons/icon.svg" alt="Toi" width="128" height="128" />
 		</a>
-		<div class='pickers-wrapper'>
-			<div class='pickers'>
-				<Datepicker
-					bind:selected={startDate}
-					on:dateSelected={pickStartCallback}
-					end={new Date()}>
-					<div class='picker__button' title='Select a ending date'>
+		<div class="pickers-wrapper">
+			<div class="pickers">
+				<Datepicker bind:selected={startDate} on:dateSelected={pickStartCallback} end={new Date()}>
+					<div class="picker__button" title="Select a ending date">
 						<span>{dateToString(dates[0])}</span>
 					</div>
 				</Datepicker>
-				<span class='pickers__to'>To</span>
-				<Datepicker
-					bind:selected={endDate}
-					on:dateSelected={pickEndCallback}
-					start={startDate}
-					end={new Date()}>
-					<div class='picker__button' title='Select a starting date'>
+				<span class="pickers__to">To</span>
+				<Datepicker bind:selected={endDate} on:dateSelected={pickEndCallback} start={startDate} end={new Date()}>
+					<div class="picker__button" title="Select a starting date">
 						<span>{dateToString(dates[1])}</span>
 					</div>
 				</Datepicker>
 			</div>
-			<p class='pickers-help'>Click the date to open a calendar</p>
+			<p class="pickers-help">Click the date to open a calendar</p>
 		</div>
 		<div>
 			<NotificationRequester />
 		</div>
 	</div>
-	<div class='block--second'>
+	<div class="block--second">
 		<SearchBox bind:searchTerm />
-		<div class='info'>
+		<div class="info">
 			<InfoBlock>
 				<a href="https://github.com/minhealthnz/nz-covid-data"> Data from the New Zealand Government </a>
 				<span style="white-space: nowrap;"
-				><a href="https://leafletjs.com" title="A JS library for interactive maps">Leaflet</a> | ©
+					><a href="https://leafletjs.com" title="A JS library for interactive maps">Leaflet</a> | ©
 					<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors</span
 				>
 			</InfoBlock>
@@ -107,7 +98,7 @@
 					</div>
 					<div class="key colour-bar" />
 				</div>
-				{#if communityPins }
+				{#if communityPins}
 					<div class="map-key">
 						<div>Community</div>
 						<div class="key community-key" />
@@ -118,166 +109,160 @@
 	</div>
 </header>
 
-<style lang='scss'>
-
+<style lang="scss">
 	.info {
-    position: absolute;
+		position: absolute;
 		top: 100%;
 		right: 0;
 		display: flex;
 		flex-flow: column;
-    align-items: flex-end;
-    .map-key {
+		align-items: flex-end;
+		.map-key {
+			display: grid;
+			grid-template-columns: 1fr 1em;
+			grid-column-gap: 0.2rem;
+			margin-top: 0.5rem;
 
-      display: grid;
-      grid-template-columns: 1fr 1em;
-      grid-column-gap: 0.2rem;
-	  margin-top: 0.5rem;
-
-      .labels {
-        text-align: right;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-      }
-      .key {
-        height: 100%;
-        width: 1em;
-        border-radius: 1rem;
-      }
-      .colour-bar {
-        background: linear-gradient(#f02b15 0%, #d751af 40%, #9171e1 80%, #2f86cc 100%);
-      }
-      .unknown-key {
-        background: #707F89;
-      }
-	  .community-key {
-		background: #ffe330;
-	  }
-    }
+			.labels {
+				text-align: right;
+				display: flex;
+				flex-direction: column;
+				justify-content: space-between;
+			}
+			.key {
+				height: 100%;
+				width: 1em;
+				border-radius: 1rem;
+			}
+			.colour-bar {
+				background: linear-gradient(#f02b15 0%, #d751af 40%, #9171e1 80%, #2f86cc 100%);
+			}
+			.unknown-key {
+				background: #707f89;
+			}
+			.community-key {
+				background: #ffe330;
+			}
+		}
 	}
 
-  header {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    z-index: 1;
+	header {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		z-index: 1;
 
-    display: flex;
-    background-color: transparent;
-  }
+		display: flex;
+		background-color: transparent;
+	}
 
-  .block--first {
+	.block--first {
 		position: relative;
-    display: flex;
-    align-items: center;
+		display: flex;
+		align-items: center;
 
-    background-color: #333333;
+		background-color: #333333;
 
-    margin-right: 5%;
-    padding: 1em;
-    z-index: 1;
+		margin-right: 5%;
+		padding: 1em;
+		z-index: 1;
 
-    &::before {
-      content: '';
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      transform: skew(-30deg) translateX(10%);
-      background-color: #333333;
-      z-index: -1;
-      box-shadow: 4px 2px 5px rgba(0,0,0,0.5);
-    }
-  }
+		&::before {
+			content: '';
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			transform: skew(-30deg) translateX(10%);
+			background-color: #333333;
+			z-index: -1;
+			box-shadow: 4px 2px 5px rgba(0, 0, 0, 0.5);
+		}
+	}
 
-  .block--second {
-    position: relative;
-    display: flex;
+	.block--second {
+		position: relative;
+		display: flex;
 		flex: 1 1 auto;
 		justify-content: flex-end;
-  }
+	}
 
 	.pickers-wrapper {
 		display: flex;
 		flex-flow: column;
-    margin: 0 1em 0 1.5em;
+		margin: 0 1em 0 1.5em;
 	}
 
 	.pickers-help {
 		font-size: 0.8em;
 		color: gray;
-    margin: 0.5em 0 0;
-  }
+		margin: 0.5em 0 0;
+	}
 
+	.pickers {
+		display: flex;
+		flex-flow: row;
+		align-items: center;
+		text-align: center;
 
-  .pickers {
-    display: flex;
-    flex-flow: row;
-    align-items: center;
-    text-align: center;
+		&__to {
+			color: white;
+			margin: 0 0.5em;
+			font-size: 1.2em;
+			font-weight: bold;
+		}
+	}
 
-    &__to {
-      color: white;
-      margin: 0 0.5em;
-      font-size: 1.2em;
-      font-weight: bold;
-    }
-  }
+	.picker__button {
+		cursor: pointer;
+		padding: 0.5em;
+		margin: 0;
 
-  .picker__button {
-    cursor: pointer;
-    padding: 0.5em;
-    margin: 0;
+		font-weight: bold;
+		border: #ffe330 2px solid;
+		background-color: #ffe330;
+		color: #222222;
+		transition: all 0.2s ease;
+		box-shadow: 0 0 10px rgba(0, 0, 0, 0);
+		border-radius: 0.25em;
 
-    font-weight: bold;
-    border: #ffe330 2px solid;
-    background-color: #ffe330;
-    color: #222222;
-    transition: all 0.2s ease;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0);
-    border-radius: 0.25em;
-
-    &:hover {
-      background-color: #ffe330;
-      box-shadow: 2px 2px 10px rgb(75, 70, 70);
-    }
+		&:hover {
+			background-color: #ffe330;
+			box-shadow: 2px 2px 10px rgb(75, 70, 70);
+		}
 
 		> span {
 			font-size: 1.2em;
 		}
-  }
+	}
 
-  .logo {
+	.logo {
+		&__image {
+			width: 64px;
+			height: 64px;
+		}
+	}
 
-    &__image {
-      width: 64px;
-      height: 64px;
-    }
-
-
-  }
-
-  @media all and (max-width: 1000px) {
-    .picker__button {
+	@media all and (max-width: 1000px) {
+		.picker__button {
 			> span {
 				font-size: 1em;
 			}
-    }
+		}
 
 		.block--first {
 			padding: 0.5em;
 		}
 
-    .pickers-wrapper {
-      margin: 0 0.7em 0 1em;
-    }
-  }
+		.pickers-wrapper {
+			margin: 0 0.7em 0 1em;
+		}
+	}
 
-  @media all and (max-width: 840px) {
-    .block--first {
+	@media all and (max-width: 840px) {
+		.block--first {
 			margin-right: 0;
 			padding-right: 2em;
 			&::before {
@@ -287,14 +272,14 @@
 
 		.block--second {
 			flex: 1 1 auto;
-      background-color: white;
-      box-shadow: 4px 2px 5px rgba(0,0,0,0.5);
+			background-color: white;
+			box-shadow: 4px 2px 5px rgba(0, 0, 0, 0.5);
 			justify-content: center;
 		}
-  }
+	}
 
 	@media all and (max-width: 715px) {
-    header {
+		header {
 			flex-flow: column;
 		}
 
@@ -304,8 +289,6 @@
 
 		.pickers-help {
 			text-align: center;
-
 		}
-  }
-
+	}
 </style>
